d368c9bdca3158fb221927c01a17cff4b180cbe4
vijay1912 <116304203+vijay1912@users.noreply.github.com>
2025-10-24 21:55:28 +0530

    Update nlb.tf

--- nlb.tf
+++ nlb.tf
@@ -3,28 +3,4 @@ resource "aws_lb_target_group" "test" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-
-  kbfkbgbsg
-
-  sgnlkngsg
-  '
-  data local_file sks
-  g
-  kgmw'g'w;gw
-  f
-  w
-  f'
-  w
-  g
-  g;
-  e'h
-  '
-  w'r
-  23'
-  23
-   {
-    filename = "${path.module}/"
-  }
-  
 }
-
